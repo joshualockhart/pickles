6a913d0f9664b10b5d93305d8a9e452740288e21
Joshua Lockhart <joshua.lockhart.14@ucl.ac.uk>
2017-10-28 16:59:27 +0100

    made an index page

--- app.py
+++ app.py
@@ -11,13 +11,9 @@ from models import Element
 
 print(os.environ['APP_SETTINGS'])
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
